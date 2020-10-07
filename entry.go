996d8feee261938c5196cc710c3c524da0c46630
Maikel Hofman <MaikelH@users.noreply.github.com>
2020-10-07 14:55:46 +0200

    Remove logging statement

--- entry.go
+++ entry.go
@@ -195,7 +195,6 @@ func getCaller() *runtime.Frame {
 
 	for f, again := frames.Next(); again; f, again = frames.Next() {
 		pkg := getPackageName(f.Function)
-		fmt.Println(pkg)
 		
 		// If the caller isn't part of this package, we're done
 		if pkg != logrusPackage {
